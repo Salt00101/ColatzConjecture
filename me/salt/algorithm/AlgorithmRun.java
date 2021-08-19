8f3ab00f65277169bd1b4ef00638656749a17424
Salt <64535929+AnupPlays@users.noreply.github.com>
2021-08-19 19:20:59 -0400

    Create AlgorithmRun.java

---
+++ me/salt/algorithm/AlgorithmRun.java
@@ -0,0 +1,9 @@
+package me.salt.algorithm;
+
+import me.salt.algorithm.utils.AlgorithmMath
+
+public class AlgorithmRun {
+    public static void main(String[] args) {
+        AlgorithmMath.Sequence()
+    }
+}
